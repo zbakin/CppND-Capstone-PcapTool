bea3053322a9e5b8cfc7cf28af3fb790736084d3
zbakin <50952098+zbakin@users.noreply.github.com>
2023-08-05 09:37:14 +0100

    Update install.sh: clean up

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,23 +3,7 @@ export WORKSPACE=/home/workspace
 
 ################ INSTALL DEPENDANCY LIBRARIES #####################
 apt install libpcap-dev flex libbison-dev tcpdump
-# apt install wireshark
-
-########################## SET UP GIT ############################
-git config --global user.email "zhanibek.bakin@gmail.com"
-git config --global user.name "Zhanibek Bakin"
-
 
 ################### INSTALL VCPKG and PCAPPLUSPLUS ###############
-# cd $WORKSPACE
-# git clone https://github.com/microsoft/vcpkg
-# cd $WORKSPACE/vcpkg
-# ./bootstrap-vcpkg.sh
-# ./vcpkg install pcapplusplus
-# cd $WORKSPACE
-# git clone https://github.com/seladb/PcapPlusPlus.git
 cd $WORKSPACE/PcapPlusPlus/
-# git checkout v22.11
-# ./configure-linux.sh --default
-# make libs
 make install
